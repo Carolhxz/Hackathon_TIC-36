301eef1f7f9a8effaa29801fb65c88093b6bf482
Carolhxz <carolinepeixoto278@gmail.com>
2024-11-24 02:50:44 -0300

    Substituindo código antigo com a nova versão

--- InitialScreen.js
+++ InitialScreen.js
@@ -8,13 +8,13 @@ export default function InitialScreen({ navigation }) {
       colors={['#4F8FCB', '#0083F1', '#01001E']}
       style={styles.container}
     >
-      {/* Logo superior esquerdo */}
+      
       <Image source={require('../Hackatoon/image/alerta.png')} style={styles.logoLeft} />
 
-      {/* Logo superior direito */}
+     
       <Image source={require('../Hackatoon/image/user2.png')} style={styles.logoRight} />
 
-      {/* Botão Sair no canto inferior esquerdo */}
+      
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('HomeScreen')}
@@ -22,50 +22,47 @@ export default function InitialScreen({ navigation }) {
         <Text style={styles.buttonText}>Sair</Text>
       </TouchableOpacity>
 
-      {/* Imagem da sirene no canto inferior direito */}
+     
       <View style={styles.imageContainer}>
         <Image source={require('../Hackatoon/image/Group 8.png')} style={styles.imageBottom} />
         <Image source={require('../Hackatoon/image/sirene.png')} style={styles.imageSirene} />
       </View>
 
-      {/* 6 Quadrados organizados em 2 colunas e 3 linhas */}
+      
       <View style={styles.squareContainer}>
-        {/* Primeiro quadrado */}
+        
         <View style={styles.square}>
           <Image source={require('../Hackatoon/image/image 2.png')} style={styles.squareImage} />
           <Text style={styles.squareTitle}>Mapa</Text>
           <Text style={styles.squareDescription}>Mapa interativo e áreas de risco</Text>
         </View>
 
-        {/* Segundo quadrado */}
         <View style={styles.square}>
           <Image source={require('../Hackatoon/image/image 14.png')} style={styles.squareImage} />
           <Text style={styles.squareTitle}>Ocorrências</Text>
           <Text style={styles.squareDescription}>Como registrar e informações</Text>
         </View>
 
-        {/* Terceiro quadrado */}
         <View style={styles.square}>
           <Image source={require('../Hackatoon/image/image 1.png')} style={styles.squareImage} />
           <Text style={styles.squareTitle}>Ferramentas</Text>
           <Text style={styles.squareDescription}>Vídeos, minicursos e jogos</Text>
         </View>
 
-        {/* Quarto quadrado */}
+       
         <View style={styles.square}>
           <Image source={require('../Hackatoon/image/image 7.png')} style={styles.squareImage} />
           <Text style={styles.squareTitle}>Comunicação</Text>
           <Text style={styles.squareDescription}>Envio de sugestões e feedback</Text>
         </View>
 
-        {/* Quinto quadrado */}
+        
         <View style={styles.square}>
           <Image source={require('../Hackatoon/image/image 10.png')} style={styles.squareImage} />
           <Text style={styles.squareTitle}>Estatísticas</Text>
           <Text style={styles.squareDescription}>Estatísticas criminais</Text>
         </View>
 
-        {/* Sexto quadrado */}
         <View style={styles.square}>
           <Image source={require('../Hackatoon/image/image 13.png')} style={styles.squareImage} />
           <Text style={styles.squareTitle}>Suporte</Text>
